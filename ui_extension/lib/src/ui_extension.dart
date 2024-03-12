330aaa0194d0579475d45824cfc9d229842d8a1b
chenhao91 <chenhao91@meituan.com>
2024-03-12 11:03:08 +0800

    ui_extension

--- ui_extension/lib/src/ui_extension.dart
+++
@@ -1,7 +0,0 @@
-/// {@template ui_extension}
-/// 我的ui.extension
-/// {@endtemplate}
-class UiExtension {
-  /// {@macro ui_extension}
-  const UiExtension();
-}
--- ui_extension/lib/ui_extension.dart
+++
@@ -1,4 +0,0 @@
-/// 我的ui.extension
-library;
-
-export 'src/ui_extension.dart';
